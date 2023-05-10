f9a5656daa8dcf7f90a95f42c5bcc6c27a1f46f2
饶长春 <raochangchun@mucang.cn>
2023-05-10 10:24:42 +0800

    配置

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         // 输出的文件名称
         filename: 'js/bundle.js',
+        
     },
     // 使用开发模式打包
     mode: "production",
